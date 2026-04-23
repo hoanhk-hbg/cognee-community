4ec6a7020c4bd8416f8a1a02b561a2ed41899bc4
yatharth <yt@usemoss.dev>
2026-04-23 15:34:03 -0700

    refactor: remove sync functions and update __all__ in Moss adapter

--- packages/vector/moss/cognee_community_vector_adapter_moss/__init__.py
+++ packages/vector/moss/cognee_community_vector_adapter_moss/__init__.py
@@ -1,15 +1,3 @@
 from .moss_adapter import MossAdapter
-from .sync import (
-    sync_session,
-    sync_session_async,
-    sync_sessions,
-    sync_sessions_async,
-)
 
-__all__ = [
-    "MossAdapter",
-    "sync_session",
-    "sync_session_async",
-    "sync_sessions",
-    "sync_sessions_async",
-]
+__all__ = ["MossAdapter"]
